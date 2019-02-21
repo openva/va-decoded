26501eb12e312f35b40c0e57ba03bdb96357f193
Waldo Jaquith <waldo@jaquith.org>
2019-02-20 21:45:06 -0500

    Replace with a lighter-weight scraper.

--- scraper.php
+++ scraper.php
@@ -1,104 +1,49 @@
 <?php
 
 /*
- * The subdirectory where all of the raw XML will be stored. Omit the trailing
- * slash.
+ * Get a list of all titles.
  */
-$output_dir = 'xml';
+$title_xml = file_get_contents('https://law.lis.virginia.gov/LawPortalWebService/xml/CodeofVAGetTitleList');
+$titles = simplexml_load_string($title_xml);
+$titles = reset($titles);
 
 /*
- * Initialize cURL.
+ * Create an array to store our sections.
  */
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+$sections = array();
 
 /*
- * Request a list of all tiles.
- */
-$url = 'http://law.lis.virginia.gov/LawPortalWebService/CodeofVAGetTitleList';
-curl_setopt($curl, CURLOPT_URL, $url);
-$xml = curl_exec($curl);
-
-if ($xml === FALSE)
-{
-	die('Fatal error: Could not retrieve a list of titles.');
-}
-
-/*
- * Turn our XML into an object.
- */
-$parser = xml_parser_create();
-xml_parse_into_struct($parser, $xml, $titles, $index);
-xml_parser_free($parser);
-
-/*
- * Iterate through the titles. Note that this isn't *really* a list of titles
- * -- it's a list of XML nodes, some of which are titles, but most of which are
- * noise, for our purposes.
+ * Iterate through each title to get a list of sections.
  */
 foreach ($titles as $title)
 {
 
-	/*
-	 * If this is a title number.
-	 */
-	if ($title['tag'] == 'TITLENUMBER')
-	{
-
-		$title_number = $title['value'];
-
-		$url = 'http://law.lis.virginia.gov/LawPortalWebService/CodeofVAGetChapterList/'
-			. $title_number;
-		curl_setopt($curl, CURLOPT_URL, $url);
-		$xml = curl_exec($curl);
-
-		if ($xml === FALSE)
-		{
-			echo 'Error: Title ' . $title_number . ' does not exist (' . $url . ')';
-		}
-
-		/*
-		 * Turn our XML into an object.
-		 */
-		$parser = xml_parser_create();
-		xml_parse_into_struct($parser, $xml, $sections, $index);
-		xml_parser_free($parser);
-
-		/*
-		 * Iterate through the chapters. Note that this isn't *really* a list
-		 * of chapters -- it's a list of XML nodes, some of which are chapters,
-		 * but most of which are noise, for our purposes.
-		 */
-		foreach ($sections as $section)
-		{
-
-			/*
-			 * If this is a section number.
-			 */
-			if ($section['tag'] == 'SECTIONNUMBER')
-			{
-
-				$section_number = $section['value'];
-
-				$url = 'http://law.lis.virginia.gov/LawPortalWebService/CodeofVAGetSectionDetails/'
-					. $section_number;
-				curl_setopt($curl, CURLOPT_URL, $url);
-				$xml = curl_exec($curl);
-
-				if ($xml === FALSE)
-				{
-					echo 'Error: Section ' . $section_number . ' does not exist ('
-						. $url . ')';
-				}
-
-				$filename = $output_dir . '/' . str_replace(':', '_', $section_number) . '.xml';
-				file_put_contents($filename, $xml);
-
-			}
-
-		}
-
-	}
+    $title_sections_xml = file_get_contents('https://law.lis.virginia.gov/LawPortalWebService/xml/CodeofVAGetChapterList/' . $title->TitleNumber);
+    $title_sections = simplexml_load_string($title_sections_xml);
+    $title_sections = reset($title_sections->ChapterList);
+    
+    foreach ($title_sections as $section)
+    {
+        $sections[] = (string) $section->SectionNumber;
+    }
 
 }
 
+/*
+ * Now we have a list of all sections, at $sections. We can now
+ * retrieve the XML for all sections.
+ */
+$output_dir = 'output';
+if (!is_dir($output_dir))
+{
+    mkdir($output_dir);
+}
+
+foreach ($sections as $section)
+{
+
+    $section_xml = file_get_contents('https://law.lis.virginia.gov/LawPortalWebService/xml/CodeofVAGetSectionDetails/' . $section);
+    file_put_contents($output_dir . '/' . $section . '.xml', $section_xml);
+    echo '.';
+    
+}
